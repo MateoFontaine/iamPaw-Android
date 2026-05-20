64405362646d7056cf7d535eeeef3b96f5ce0c47
Mateo Fontaine <mateofontaine.7@gmail.com>
2026-05-19 23:12:27 -0300

    feat: agregar catalogo de rutas (Screen) para navegacion

--- app/src/main/java/com/example/iampaw/MainActivity.kt
+++ app/src/main/java/com/example/iampaw/MainActivity.kt
@@ -4,44 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.iampaw.ui.theme.IamPawTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Esto hace que la app ocupe toda la pantalla (hasta la barra de estado arriba)
         enableEdgeToEdge()
+
         setContent {
             IamPawTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                // Acá adentro vamos a inyectar el NavigationStack en el próximo paso
+                // tal cual como lo hizo el profe Gladkoff.
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IamPawTheme {
-        Greeting("Android")
     }
 }
